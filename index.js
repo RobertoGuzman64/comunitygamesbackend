944b4bebf5102818f3abfa6dc05fe75322716b6d
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-29 14:39:26 +0200

    feat: Añadidas las opciones de express, cors y base de datos en el archivo index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./db.js');
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+
+let opcionesCors = {//CONFIGURO OPCIONES DE CORS
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+//Middleware
+app.use(express.json()); //PUEDO OBTENER JSON DEL BODY
+app.use(cors(opcionesCors));  //USO CORS
+app.use(router);
+
+app.listen(PORT, ()=> console.log(`Servidor levantado en el puerto ${PORT}`));
+// db.then(()=>{
+//     app.listen(PORT, ()=> console.log(`Servidor en el puerto ${PORT}`)); //Conectado a la base de datos
+// })
+// .catch((err)=> console.log(err.message));
