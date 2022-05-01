7205f0595bafff2f5960673e2e856aeab7167b2e
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-01 18:55:51 +0200

    feat: Añadido nuevo método de crearUsuario en UsuarioController.js

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -3,6 +3,21 @@ const { Op } = require("sequelize");
 const bcrypt = require('bcrypt');
 const authConfig = require('../config/auth');
 const jwt = require('jsonwebtoken');
-const UsuarioController = {};
 
+class MetodosUsuario {
+    constructor() {
+    }
+    // Método de crear un Usuario con contraseña encriptada.
+    async crearUsuario(body) {
+        body.clave = bcrypt.hashSync(body.clave, Number.parseInt(authConfig.rondas));
+        let usuarioNuevo = await Usuario.create(body).then(usuarioNuevo => {
+            return { status: 201, datos: usuarioNuevo }
+        }).catch(error => {
+            return { status: 400, datos: { error: error.message } }
+        });
+        return usuarioNuevo;
+    }
+
+}
+let UsuarioController = new MetodosUsuario();
 module.exports = UsuarioController;
--- views/UsuarioRouter.js
+++ views/UsuarioRouter.js
@@ -4,7 +4,21 @@ const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 const UsuarioController = require('../controllers/UsuarioController');
 
+// POST
+// Endpoint de crear un Usuario.
+// http://localhost:5000/usuarios
 
+router.post("/", async (req, res) => {
+    try {
+        const body = req.body;
+        let respuesta = await UsuarioController.crearUsuario(body);
+        res.status(respuesta.status).json(respuesta.datos);
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+});
 
 
 
