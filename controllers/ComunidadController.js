27b6e6afee8c2e6c6a685e0d8c36b0983695ff67
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-02 23:46:02 +0200

    feat: Añadidos nuevos endpoints y funciónes en ComunidadRouter.js y ComunidadController.js

--- controllers/ComunidadController.js
+++ controllers/ComunidadController.js
@@ -5,7 +5,44 @@ const authConfig = require('../config/auth');
 const jwt = require('jsonwebtoken');
 const ComunidadController = {};
 
+// Función de mostrar todas las Comunidades.
+ComunidadController.verComunidades = (req, res) => {
+    Comunidad.findAll()
+        .then(data => {
+            res.send(data)
+        });
+};
 
+// Función de ver una Comunidad por ID.
+ComunidadController.verComunidadId = (req, res) => {
+    let id = req.params.id;
+    Comunidad.findOne({
+        where: { id: id }
+    }).then(data => {
+        res.send(data)
+    });
+};
+
+// Función de crear una Comunidad.
+ComunidadController.crearComunidad = (req, res) => {
+    let titulo = req.body.titulo;
+    let imagen = req.body.imagen;
+    let genero = req.body.genero;
+    let fecha = req.body.fecha;
+    let popularidad = req.body.popularidad;
+    let descripcion = req.body.descripcion;
+    let id_usuario = req.body.id_usuario;
+    Comunidad.create({
+        titulo: titulo,
+        imagen: imagen,
+        genero: genero,
+        fecha: fecha,
+        popularidad : popularidad,
+        descripcion : descripcion,
+    }).then(data => {
+        res.send(data)
+    });
+};
 
 
 
--- views/ComunidadRouter.js
+++ views/ComunidadRouter.js
@@ -4,6 +4,29 @@ const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 const ComunidadController = require('../controllers/ComunidadController');
 
+// Endpoint de mostrar todas las Comunidades.
+router.get('/', auth, ComunidadController.verComunidades);
+// http://localhost:5000/comunidades
+
+// Endpoint de ver una Comunidad por ID.
+router.get('/:id', auth, isAdmin, ComunidadController.verComunidadId);
+// http://localhost:5000/comunidades/id
+
+// Endpoint de crear una Comunidad.
+router.post('/', auth, isAdmin, ComunidadController.crearComunidad);
+// http://localhost:5000/comunidades
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
