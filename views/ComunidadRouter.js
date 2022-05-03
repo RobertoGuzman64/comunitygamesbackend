af5ed718de647dacf64bee50ebef6bcfc001c7c7
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-03 15:41:29 +0200

    feat: Añadidos los endpoints necesarios de comunidades en ComunidadRouter.js

--- views/ComunidadRouter.js
+++ views/ComunidadRouter.js
@@ -5,19 +5,28 @@ const isAdmin = require("../middlewares/isAdmin");
 const ComunidadController = require('../controllers/ComunidadController');
 
 // Endpoint de mostrar todas las Comunidades.
-router.get('/', auth, ComunidadController.verComunidades);
+router.get('/', ComunidadController.verComunidades);
 // http://localhost:5000/comunidades
 
 // Endpoint de ver una Comunidad por ID.
-router.get('/:id', auth, isAdmin, ComunidadController.verComunidadId);
+router.get('/:id', auth, ComunidadController.verComunidadId);
 // http://localhost:5000/comunidades/id
 
 // Endpoint de crear una Comunidad.
 router.post('/', auth, isAdmin, ComunidadController.crearComunidad);
+// http://localhost:5000/comunidades/
+
+// Endpoint de Modificar la Comunidad por ID.
+router.put('/:id', auth, isAdmin, ComunidadController.modificarComunidad);
+// http://localhost:5000/comunidades/:id
+
+// Endpoint de eliminar todas las Comunidades.
+router.delete('/', auth, isAdmin, ComunidadController.borrarComunidades);
 // http://localhost:5000/comunidades
 
-
-
+// Endpoint de eliminar una Comunidad por ID.
+router.delete('/:pk', auth, isAdmin, ComunidadController.borrarComunidadId);
+// http://localhost:5000/comunidades/:id
 
 
 
