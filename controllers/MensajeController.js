699de3fb3fe9e86947c674c772444992894179fe
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-04 14:13:12 +0200

    feat: Añadidos algunos endpoints y funciones de Mensaje en MensajeRouter.js y MensajeController.js

--- controllers/MensajeController.js
+++ controllers/MensajeController.js
@@ -29,6 +29,103 @@ MensajeController.verMensajeId = (req, res) => {
     }
 }
 
+// Función de crear un Mensaje como Miembro a una comunidad.
+MensajeController.crearMensajeMiembro = (req, res) => {
+    try {
+        let comunidad_id = req.body.comunidad_id;
+        let miembro_id = req.body.miembro_id;
+        let mensaje = req.body.mensaje;
+        let fecha = req.body.fecha;
+        let hora = req.body.hora;
+        Mensaje.create({
+            comunidad_id: comunidad_id,
+            miembro_id: miembro_id,
+            mensaje: mensaje,
+            fecha: fecha,
+            hora: hora
+        })
+            .then(datos => {
+                res.send(datos);
+            });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+// Función de crear un Mensaje privado como Amigo.
+MensajeController.crearMensajeAmigo = (req, res) => {
+    try {
+        let amigo_id = req.body.amigo_id;
+        let mensaje = req.body.mensaje;
+        let fecha = req.body.fecha;
+        let hora = req.body.hora;
+        Mensaje.create({
+            amigo_id: amigo_id,
+            mensaje: mensaje,
+            fecha: fecha,
+            hora: hora
+        })
+            .then(datos => {
+                res.send(datos);
+            });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+// Función de modificar un Mensaje de un Miembro a una comunidad. CREO QUE FALTA EL ID DEL MENSAJE let id = req.params.id;
+MensajeController.modificarMensajeMiembro = (req, res) => {
+    let comunidad_id = req.params.comunidad_id;
+    let miembro_id = req.params.miembro_id;
+    let mensaje = req.body.mensaje;
+    let fecha = req.body.fecha;
+    let hora = req.body.hora;
+    try {
+        Mensaje.update({
+            comunidad_id: comunidad_id,
+            miembro_id: miembro_id,
+            mensaje: mensaje,
+            fecha: fecha,
+            hora: hora
+        }, {
+                where: {
+                    comunidad_id: comunidad_id,
+                    miembro_id: miembro_id
+                }
+            })
+            .then(datos => {
+                res.send(datos);
+            });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+// Función de modificar un Mensaje privado de un Amigo.  CREO QUE FALTA EL ID DEL MENSAJE let id = req.params.id;
+MensajeController.modificarMensajeAmigo = (req, res) => {
+    let amigo_id = req.params.amigo_id;
+    let mensaje = req.body.mensaje;
+    let fecha = req.body.fecha;
+    let hora = req.body.hora;
+    try {
+        Mensaje.update({
+            amigo_id: amigo_id,
+            mensaje: mensaje,
+            fecha: fecha,
+            hora: hora
+        }, {
+                where: {
+                    amigo_id: amigo_id
+                }
+            })
+            .then(datos => {
+                res.send(datos);
+            });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
 
 
 
--- views/MensajeRouter.js
+++ views/MensajeRouter.js
@@ -12,9 +12,25 @@ router.get('/', auth, isAdmin, MensajeController.verMensajes);
 router.get('/:id', auth, MensajeController.verMensajeId);
 // http://localhost:5000/mensajes/id
 
+// Endpoint de crear un Mensaje como Miembro a una Comunidad.
+router.post('/miembro', auth, MensajeController.crearMensajeMiembro);
+// http://localhost:5000/mensajes/miembro
 
+// Endpoint de crear un Mensaje privado como Amigo.
+router.post('/amigo', auth, MensajeController.crearMensajeAmigo);
+// http://localhost:5000/mensajes/amigo
 
+// Endpoint de modificar un Mensaje de un Miembro de Comunidad.
+router.put('/miembro/:id/:id/:id', auth, MensajeController.modificarMensajeMiembro);
+// http://localhost:5000/mensajes/miembro/:id/:id
 
+// Endpoint de modificar un Mensaje privado de un Amigo.
+router.put('/amigo/:id/:id/:id', auth, MensajeController.modificarMensajeAmigo);
+// http://localhost:5000/mensajes/amigo/:id/:id
+
+// Endpoint de eliminar un Mensaje de un Miembro de Comunidad.
+router.delete('/miembro/:id/:id/:id', auth, MensajeController.borrarMensajeMiembro);
+// http://localhost:5000/mensajes/miembro/:id/:id/:id
 
 
 
