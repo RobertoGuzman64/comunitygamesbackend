5c38c5520b958c45a734a5f1c95524e51b169b3a
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-17 17:14:03 +0200

    feat: intento de deploy de heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('./db.js');
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
-let opcionesCors = {//CONFIGURO OPCIONES DE CORS
+let opcionesCors = {//CONFIGURO OPCIONES DE CORS PARA QUE PUEDA ACCEDER A TODOS LOS DOMINIOS
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
